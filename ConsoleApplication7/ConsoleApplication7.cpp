b309ac7ae8f31d82632902a25ffaee76e29f19a1
HYPERPC <HYPERPC@DESKTOP-MU1U2GL>
2023-03-27 21:26:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,69 @@
+﻿
+#include<Windows.h>
+#include<string>
+#include<iomanip>
+#include<conio.h>
+#include<fstream>
+#include <iostream>
+using namespace std;
+
+class Pet
+{
+	char* poroda; 
+	uint32_t ves ;
+	char* okpc; 
+
+public:
+	
+
+	Pet()
+	{
+		cout << " "
+			<< endl; 
+	}
+	const char* getPoroda()
+	{
+		return poroda; 
+	}
+	const char* getOkpc()
+	{
+		return okpc; 
+	}
+
+	uint32_t getVes()
+	{
+		return ves; 
+	}
+	void setPoroda(const char* Pet); 
+	void setPoroda(const char* Pet);
+	void setVes(uint32_t Pet); 
+	
+	Pet operator=(Pet&& poroda )
+	{
+		if (this != &poroda)
+		{
+					}
+		
+	}
+	
+
+	Pet(const Pet&); 
+	Pet& operator=(const Pet ); 
+
+	~Pet(void)
+	{
+		if (poroda != NULL)
+			delete[]poroda;
+	}
+};
+
+
+
+int main()
+{
+   
+	
+
+}
+
+
